51b40ef7234029ff4306e5b0f1ba59f3923b24ec
akira-iguchi <akituri931@icloud.com>
2021-02-24 23:55:57 +0900

    vue

---
+++ vue.js
@@ -0,0 +1,44 @@
+var fish = new Vue({
+    el: "#app2",
+
+    data: {
+        newItem: "",
+        todos: []
+    },
+
+    methods: {
+        addItem: function(event) {
+            if(this.newItem == '') return;
+            // returnで処理が終了
+
+            var todo = {
+                item: this.newItem,
+                isDone: false
+            };
+            this.todos.push(todo);
+            // thisは両方ともdataを指す（dataのtodosの値を参照）
+            this.newItem = '';
+        },
+        deleteItem: function(index) {
+            alert(index);
+        }
+    }
+})
+
+
+// var app = new Vue({
+//     el: '#app1',
+
+//     data: {
+//         message: "hello",
+//         url: "aaa.com",
+//         toggle: true,
+//         languages: ["japan", "America"]
+//     },
+
+//     methods: {
+//         onclick: function() {
+//             this.message = "fishing"
+//         }
+//     }
+// })
