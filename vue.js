717bcf3e758cf6189d324b60a5535234c5269339
akira-iguchi <akituri931@icloud.com>
2021-02-25 09:17:44 +0900

    component

--- vue.js
+++ vue.js
@@ -1,29 +1,43 @@
-var app = new Vue({
+Vue.component("hello", {
+    template: '<p>hello</p>'
+})
+
+var app = new Vue ({
     el: "#app",
 
-    data: {
-        newItem: "",
-        todos: []
-    },
 
-    methods: {
-        addItem: function(event) {
-            if(this.newItem == '') return;
-            // returnで処理が終了
-
-            var todo = {
-                item: this.newItem,
-                isDone: false
-            };
-            this.todos.push(todo);
-            // thisは両方ともdataを指す（dataのtodosの値を参照）
-            this.newItem = '';
-        },
-        deleteItem: function(index) {
-            this.todos.splice(index, 1)
-        }
-    }
 })
+
+
+
+
+
+// var app = new Vue({
+//     el: "#app",
+
+//     data: {
+//         newItem: "",
+//         todos: []
+//     },
+
+//     methods: {
+//         addItem: function(event) {
+//             if(this.newItem == '') return;
+//             // returnで処理が終了
+
+//             var todo = {
+//                 item: this.newItem,
+//                 isDone: false
+//             };
+//             this.todos.push(todo);
+//             // thisは両方ともdataを指す（dataのtodosの値を参照）
+//             this.newItem = '';
+//         },
+//         deleteItem: function(index) {
+//             this.todos.splice(index, 1)
+//         }
+//     }
+// })
 
 
 // var app = new Vue({
